e993194b202a619113eb554903c402abac7ca37f
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2018-06-11 23:56:35 +0300

    update reactor-netty to 0.7.8.RELEASE, fixing Websocket client critical bug https://github.com/reactor/reactor-netty/issues/345, which prevented RSocket Netty-Websocket transport from sending its close event via onClose()

--- test/src/test/kotlin/io/rsocket/kotlin/test/transport/NettyWebsocketEndToEndTest.kt
+++ test/src/test/kotlin/io/rsocket/kotlin/test/transport/NettyWebsocketEndToEndTest.kt
@@ -1,17 +1,8 @@
 package io.rsocket.kotlin.test.transport
 
-import io.rsocket.kotlin.test.transport.EndToEndTest
 import io.rsocket.kotlin.transport.netty.client.WebsocketClientTransport
 import io.rsocket.kotlin.transport.netty.server.WebsocketServerTransport
 
 class NettyWebsocketEndToEndTest : EndToEndTest(
         { WebsocketClientTransport.create(it) },
-        { WebsocketServerTransport.create(it.port) }) {
-
-    /*noop until https://github.com/reactor/reactor-netty/pull/346 is released*/
-    override fun close() {
-    }
-
-    override fun closedAvailability() {
-    }
-}
+        { WebsocketServerTransport.create(it.port) })
