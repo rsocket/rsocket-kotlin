f247c895c4cbdf4a747e3df1452c73c41160f996
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2019-09-01 16:19:51 +0300

    git based versioning (#71)

--- ci/buildViaTravis.sh
+++ ci/buildViaTravis.sh
@@ -3,14 +3,14 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew build
+  ./gradlew -Pbranch="${TRAVIS_BRANCH}" build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" build artifactoryPublish --stacktrace
+  ./gradlew -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -Pbranch="${TRAVIS_BRANCH}" build artifactoryPublish --stacktrace
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew -Pversion="$TRAVIS_TAG" -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" build bintrayUpload --stacktrace
+  ./gradlew -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" build bintrayUpload --stacktrace
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew build
+  ./gradlew -Pbranch="${TRAVIS_BRANCH}" build
 fi
