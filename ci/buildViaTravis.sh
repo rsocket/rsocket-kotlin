f101ee113f07a12549ffd0adf1deda14b8ae397d
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2019-09-01 16:57:00 +0300

    hubflow friendly scripts

--- ci/buildViaTravis.sh
+++ ci/buildViaTravis.sh
@@ -5,12 +5,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   ./gradlew -Pbranch="${TRAVIS_BRANCH}" build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
-  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+  echo -e 'Build Branch => Branch ['$TRAVIS_BRANCH']'
   ./gradlew -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -Pbranch="${TRAVIS_BRANCH}" build artifactoryPublish --stacktrace
-elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" build bintrayUpload --stacktrace
 else
-  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew -Pbranch="${TRAVIS_BRANCH}" build
+  echo -e 'Build Tag => Tag ['$TRAVIS_TAG']'
+  ./gradlew build
 fi
