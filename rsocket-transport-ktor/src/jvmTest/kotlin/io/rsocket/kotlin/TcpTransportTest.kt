01c147e15a56ad722d0018aede4f568125b653cb
Oleg Yukhnevich <whyoleg@gmail.com>
2020-08-31 17:19:58 +0300

    trying to fix TCP tests freeze on CI

--- rsocket-transport-ktor/src/jvmTest/kotlin/io/rsocket/kotlin/TcpTransportTest.kt
+++ rsocket-transport-ktor/src/jvmTest/kotlin/io/rsocket/kotlin/TcpTransportTest.kt
@@ -18,20 +18,42 @@ package io.rsocket.kotlin
 
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
+import io.ktor.util.*
 import io.rsocket.kotlin.connection.*
-import io.rsocket.kotlin.core.*
 import kotlinx.coroutines.*
+import kotlin.test.*
 
+@OptIn(InternalAPI::class)
 class TcpTransportTest : TransportTest() {
-    override suspend fun init(): RSocket = builder.connect("127.0.0.1", 2323).connection.connectClient()
+    private val selector = SelectorManager(Dispatchers.IO)
+    private val builder = aSocket(selector).tcp()
+    private val server = builder.bind("127.0.0.1", 2323)
 
-    companion object {
-        private val builder = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp()
-
-        init {
-            GlobalScope.launch(start = CoroutineStart.UNDISPATCHED) {
-                builder.bind("127.0.0.1", 2323).rSocket { TestRSocket() }
-            }
+    @BeforeTest
+    fun setup() {
+        GlobalScope.launch(start = CoroutineStart.UNDISPATCHED) {
+            server.accept().connection.startServer { TestRSocket() }.join()
         }
     }
+
+    @AfterTest
+    fun cleanup() {
+        server.close()
+        selector.close()
+        runBlocking { server.socketContext.join() }
+    }
+
+    override suspend fun init(): RSocket {
+        lateinit var error: Throwable
+        repeat(5) {
+            try {
+                return builder.connect("127.0.0.1", 2323).connection.connectClient()
+            } catch (e: Throwable) {
+                error = e
+                delay(500) //sometimes address isn't yet available
+            }
+        }
+        throw error
+    }
+
 }
