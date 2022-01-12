b5c0915b6659dd695ba9735904ba44eb5bae2fd7
olme04 <olme04@zohomail.eu>
2022-01-12 10:04:25 +0000

    Setup jvm toolchain * compile using JDK 8 * able to run tests on JDK 8, 11 and 17

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,3 +6,10 @@ dependencies {
     implementation(buildLibs.build.kotlin)
     implementation(buildLibs.build.kotlinx.atomicfu)
 }
+
+kotlin {
+    jvmToolchain {
+        this as JavaToolchainSpec
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
--- buildSrc/src/main/kotlin/targetsJvm.kt
+++ buildSrc/src/main/kotlin/targetsJvm.kt
@@ -1,7 +1,19 @@
+import org.gradle.jvm.toolchain.*
+import org.gradle.kotlin.dsl.*
 import org.jetbrains.kotlin.gradle.dsl.*
 
 fun KotlinMultiplatformExtension.configureJvm(block: TargetBuilder.() -> Unit = {}) {
+    jvmToolchain { (this as JavaToolchainSpec).setJdk(8) }
     jvm {
+        listOf(11, 17).forEach { jdkVersion ->
+            testRuns.create("${jdkVersion}Test") {
+                executionTask.configure {
+                    javaLauncher.set(
+                        project.extensions.getByType<JavaToolchainService>().launcherFor { setJdk(11) }
+                    )
+                }
+            }
+        }
         testRuns.all {
             executionTask.configure {
                 // ActiveProcessorCount is used here, to make sure local setup is similar as on CI
@@ -11,4 +23,8 @@ fun KotlinMultiplatformExtension.configureJvm(block: TargetBuilder.() -> Unit = 
         }
     }
     TargetBuilder(sourceSets, "jvm").block()
+}
+
+private fun JavaToolchainSpec.setJdk(version: Int) {
+    languageVersion.set(JavaLanguageVersion.of(version))
 }
