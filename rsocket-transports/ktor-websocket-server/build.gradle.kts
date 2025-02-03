151aff59fbd862c6f3552efe8b653a0c9b60abdb
Oleg Yukhnevich <whyoleg@gmail.com>
2025-02-03 08:43:25 +0200

    Cleanup ktor deps

--- rsocket-transports/ktor-websocket-server/build.gradle.kts
+++ rsocket-transports/ktor-websocket-server/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2024 the original author or authors.
+ * Copyright 2015-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ kotlin {
             implementation(projects.rsocketTransportKtorWebsocketInternal)
             implementation(projects.rsocketInternalIo)
             api(projects.rsocketCore)
-            api(libs.ktor.server.host.common)
+            api(libs.ktor.server.core)
             api(libs.ktor.server.websockets)
         }
         commonTest.dependencies {
@@ -39,10 +39,6 @@ kotlin {
             implementation(projects.rsocketTransportKtorWebsocketClient)
             implementation(libs.ktor.client.cio)
             implementation(libs.ktor.server.cio)
-        }
-        jvmTest.dependencies {
-            implementation(libs.ktor.client.okhttp)
-            implementation(libs.ktor.server.netty)
         }
     }
 }
