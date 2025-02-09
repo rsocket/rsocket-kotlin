b5e7adc37208d582b84367d89b44a5ad3a3b2d65
Oleg Yukhnevich <whyoleg@gmail.com>
2025-02-09 19:49:24 +0200

    [EAP] Ktor 3.1

--- build-settings/src/main/kotlin/rsocketsettings.repositories.settings.gradle.kts
+++ build-settings/src/main/kotlin/rsocketsettings.repositories.settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2024 the original author or authors.
+ * Copyright 2015-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
         mavenCentral()
     }
 }
